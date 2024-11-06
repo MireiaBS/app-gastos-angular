9c67987fbacdde93af2c2bcae1b483ec0f6e3d49
MireiaBs <mireia__24@hotmail.com>
2024-11-06 10:48:48 +0100

    Primera pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavUserComponent } from "./components/nav-user/nav-user.component";
+import { MainPageComponent } from "./components/main-page/main-page.component";
+import { ExpenseModalComponent } from './components/expense-modal/expense-modal.component';
+import { ExpenseItemComponent } from './components/expense-item/expense-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavUserComponent, MainPageComponent, ExpenseModalComponent, ExpenseItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prova';
+  title = 'App gastos';
 }
