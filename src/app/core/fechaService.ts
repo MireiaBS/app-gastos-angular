d76ffb47b82414be5fbb5c751e03967263a4b8af
MireiaBs <mireia__24@hotmail.com>
2024-12-02 16:12:25 +0100

    Vistas

--- src/app/core/fechaService.ts
+++ src/app/core/fechaService.ts
@@ -14,29 +14,18 @@ export class FechaService {
   constructor(private firestore: Firestore) {
 
   }
-
-
- /*  getFechaActual() {
-    const fechaActual: Date = new Date();
-    const dia = fechaActual.getDate();
-    const mes = fechaActual.toLocaleDateString('es-ES', { month: 'long' });
-    const anio = fechaActual.getFullYear();
-
-    const mesCapitalizado = this.capitalizarMes(mes);
-    const fechaFormateada : string = `${dia} de ${mesCapitalizado} del ${anio}`
-
-    return fechaFormateada;
-  }
-
-  capitalizarMes(mes : string) {
-    const mesCapitalizado = mes.charAt(0).toUpperCase() + mes.slice(1).toLowerCase();
-    return mesCapitalizado;
-  }*/
-
   getGastosByFecha(f1: string): Observable<Gasto[]> {
     const gastosRef = collection(this.firestore, `gastos`);
     const arrayGastos = query(gastosRef, where ('fecha', '==', f1))
     //console.log('getGastosByFecha(): ', arrayGastos)
+    return collectionData(arrayGastos, { idField: 'id' }) as Observable<Gasto[]>;
+  }
+
+  getGastosByRange(fechaInicio: string, fechaFin: string): Observable<Gasto[]> {
+    const gastosRef = collection(this.firestore, `gastos`);
+    const arrayGastos = query(gastosRef,
+      where('fecha', '>=', fechaInicio),
+      where('fecha', '<=', fechaFin));
     return collectionData(arrayGastos, { idField: 'id' }) as Observable<Gasto[]>;
   }
 
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,11 +1,10 @@
-import { Component, ElementRef, ViewChild, } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Gasto } from '../classes/gasto';
 import { CommonModule } from '@angular/common';
 import { Categoria } from '../classes/categoria';
 import { DataService } from '../core/dataService';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FechaService } from '../core/fechaService';
-
 
 @Component({
   selector: 'inicio',
@@ -20,76 +19,80 @@ export class InicioComponent {
   gastos: Gasto[] = [];
   categorias: Categoria[] = [];
   fecha: Date = new Date();
-  fechaString : string = '';
-  fechaFormateada : string = '';
-
-
+  fechaString: string = '';
+  fechaFormateada: string = '';
 
   constructor(
     private fechaService: FechaService,
     private dataService: DataService,
     private router: Router,
-    private route: ActivatedRoute,
-  ) {
-
-
-  }
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.obtenerDatos();
-    this.fechaFormateada = this.formatearFecha(this.fecha)
+    this.fechaFormateada = this.formatearFecha(this.fecha);
   }
 
   ngAfterViewInit() {
     this.setFechaActualInput();
-    this.obtenerGastosPorFecha(this.fechaInput.nativeElement.value)
+    this.obtenerGastosPorFecha(this.fechaInput.nativeElement.value);
   }
 
   // EVENTOS
 
   inputFechaSeleccionada_onChange() {
-    const fechaSeleccionada : string = this.fechaInput.nativeElement.value;
-    this.obtenerGastosPorFecha(fechaSeleccionada);
+    this.obtenerGastosPorFecha(this.fechaInput.nativeElement.value);
   }
 
-  btnMostrarVistaDia_onClick() {}
+  btnMostrarVistaHoy_onClick() {
+    const fechaActual = new Date();
+    const fechaString = this.formatearFechaInput(fechaActual);
+    this.fechaInput.nativeElement.value = fechaString;
+    this.fechaFormateada = this.formatearFecha(fechaActual);
+    this.obtenerGastosPorFecha(fechaString);
+  }
 
   btnMostrarVistaSemana_onClick() {
+    const fechaSeleccionada = this.fechaInput.nativeElement.value;
+    const fecha = new Date(fechaSeleccionada);
+    const { fechaInicio, fechaFin } = this.calcularRangoSemana(fecha);
+    this.obtenerGastosPorRango(this.formatearFechaInput(fechaInicio), this.formatearFechaInput(fechaFin));
+
+  }
+
+  btnMostrarVistaMes_onClick() {
+    const fechaSeleccionada = this.fechaInput.nativeElement.value;
+    const fecha = new Date(fechaSeleccionada);
+    const { fechaInicio, fechaFin } = this.calcularRangoMes(fecha);
+    this.obtenerGastosPorRango(this.formatearFechaInput(fechaInicio), this.formatearFechaInput(fechaFin));
 
   }
 
   btnDiaAnterior_onClick() {
-    const fechaSeleccionada : string = this.fechaInput.nativeElement.value;
+    this.cambiarFecha(-1);
+    const fechaSeleccionada: string = this.fechaInput.nativeElement.value;
     const [anio, mes, dia] = fechaSeleccionada.split('-');
-    const fecha : Date = new Date(parseInt(anio), parseInt(mes) - 1, parseInt(dia));
+    const fecha: Date = new Date(
+      parseInt(anio),
+      parseInt(mes) - 1,
+      parseInt(dia)
+    );
 
     fecha.setDate(fecha.getDate() - 1);
-    this.actualizarFecha(fecha);
-  }
-
-  actualizarFecha(fecha: Date){
-    const diaAnterior = String(fecha.getDate()).padStart(2,'0');
-    const mesAnterior = String(fecha.getMonth() + 1).padStart(2, '0');
-    const anioAnterior = fecha.getFullYear();
-    const fechaString = `${anioAnterior}-${mesAnterior}-${diaAnterior}`;
-
-    this.fechaInput.nativeElement.value = fechaString;
-    this.fechaFormateada = this.formatearFecha(fecha);
-
-    this.obtenerGastosPorFecha(fechaString);
   }
 
   btnDiaPosterior_onClick() {
-    const fechaSeleccionada : string = this.fechaInput.nativeElement.value;
+    this.cambiarFecha(1);
+    const fechaSeleccionada: string = this.fechaInput.nativeElement.value;
     const [anio, mes, dia] = fechaSeleccionada.split('-');
-    const fecha : Date = new Date(parseInt(anio), parseInt(mes) - 1, parseInt(dia));
+    const fecha: Date = new Date(
+      parseInt(anio),
+      parseInt(mes) - 1,
+      parseInt(dia)
+    );
 
     fecha.setDate(fecha.getDate() + 1);
-    this.actualizarFecha(fecha);
-  }
-
-  btnMostrarVistaMes_onClick() {
-
   }
 
   itemGasto_onClick(gasto: Gasto) {
@@ -99,14 +102,7 @@ export class InicioComponent {
   // FUNCIONES
 
   obtenerDatos() {
-    /*  const f1 = new Date();
-    const f2 = new Date();
-    this.obtenerGastosPorFecha(f1, f2); */
     this.obtenerCategorias();
-
-    //this.obtenerGastos();
-
-
   }
 
   obtenerCategorias() {
@@ -119,62 +115,98 @@ export class InicioComponent {
     return this.categorias.find((categoria) => categoria.id == categoriaId);
   }
 
-  /* obtenerGastos() {
-    this.dataService.getGastos().subscribe((gastos) => {
-      console.log('gastos:', gastos);
-      this.gastos = gastos;
-    });
+  sumarImportes() {
+    return this.gastos.reduce((sum, gasto) => sum + gasto.importe, 0);
+  }
 
-  } */
+  //FECHAS
 
-  sumarImportes(){
-    return this.gastos.reduce((sum,gasto) => sum + gasto.importe, 0);
+  obtenerGastosPorFecha(fechaSeleccionada: string) {
+    this.fechaService
+      .getGastosByFecha(fechaSeleccionada)
+      .subscribe((gastos) => {
+        this.gastos = gastos;
+        this.fechaFormateada = this.formatearFecha(new Date(fechaSeleccionada));
+       // console.log('fecha setFecha2: ', this.fechaFormateada);
+      });
+  }
+
+  obtenerGastosPorRango(inicio : string, fin : string) {
+    this.fechaService
+      .getGastosByRange(inicio, fin)
+      .subscribe((gastos) => {
+        this.gastos = gastos;
+        const fechaFormateadaInicio = this.formatearFecha(inicio);
+        const fechaFormateadaFin = this.formatearFecha(fin);
+        this.fechaFormateada = `${fechaFormateadaInicio} al ${fechaFormateadaFin}`;
+       // console.log('Rango seleccionado', this.fechaFormateada)
+      }
+    )
 
   }
 
   setFechaActualInput() {
-
-      const fecha: Date = new Date();
-      const dia = String(fecha.getDate()).padStart(2,'0');
-      const mes = String(fecha.getMonth()+1).padStart(2,'0');
-      const anio = fecha.getFullYear();
-      const fechaFormateada =`${anio}-${mes}-${dia}`;
-      this.fechaInput.nativeElement.value = fechaFormateada;
-
+    if (this.fechaInput && this.fechaInput.nativeElement) {
+      const fechaActual = new Date();
+      this.fechaInput.nativeElement.value = this.formatearFechaInput(fechaActual);
+      this.fechaString = this.fechaInput.nativeElement.value;
+    }
   }
 
-  obtenerGastosPorFecha(fechaSeleccionada: string) {
-    this.fechaService.getGastosByFecha(fechaSeleccionada)
-    .subscribe((gastos)=> {
-      this.gastos = gastos})
-      ;
-      const [anio, mes, dia] = fechaSeleccionada.split('-');
-      const fecha = new Date(parseInt(anio), parseInt(mes) - 1, parseInt(dia));
-      this.fechaFormateada = this.formatearFecha(fecha)
+  cambiarFecha(dias: number) {
+    const fechaSeleccionada: string = this.fechaInput.nativeElement.value;
+    const [anio, mes, dia] = fechaSeleccionada.split('-').map(Number);
+    const fecha = new Date(anio, mes - 1, dia);
+    fecha.setDate(fecha.getDate() + dias);
 
+    const nuevaFecha = this.formatearFechaInput(fecha);
+    this.fechaInput.nativeElement.value = nuevaFecha;
+    this.fechaFormateada = this.formatearFecha(fecha);
+    this.obtenerGastosPorFecha(nuevaFecha);
   }
 
-  formatearFecha(fecha: Date) {
-   // console.log('formateo: ',fecha)
-    const dia = fecha.getDate();
-    const mes = fecha.toLocaleDateString('es-ES', { month: 'long' });
-    const anio = fecha.getFullYear();
+  formatearFecha(fecha: Date | string): string {
+    const dateOf = typeof fecha === 'string' ? new Date(fecha) : fecha;
+
+    const dia = dateOf.getDate();
+    const mes = dateOf.toLocaleDateString('es-ES', { month: 'long' });
+    const anio = dateOf.getFullYear();
 
     const mesCapitalizado = this.capitalizarMes(mes);
-    const fechaFormateada : string = `${dia} de ${mesCapitalizado} del ${anio}`
+    const fechaFormateada: string = `${dia} de ${mesCapitalizado} del ${anio}`;
 
-    //console.log('fecha formateada: ',fechaFormateada)
     return fechaFormateada;
   }
 
-  capitalizarMes(mes : string) {
-    const mesCapitalizado = mes.charAt(0).toUpperCase() + mes.slice(1).toLowerCase();
+  formatearFechaInput(fecha: Date): string {
+    const dia = String(fecha.getDate()).padStart(2, '0');
+    const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+    const anio = fecha.getFullYear();
+    return `${anio}-${mes}-${dia}`;
+  }
+
+  capitalizarMes(mes: string) {
+    const mesCapitalizado =
+      mes.charAt(0).toUpperCase() + mes.slice(1).toLowerCase();
     return mesCapitalizado;
   }
 
-  pasarStringToDate(fecha : string) {
-    const fechaString = new Date ();
+  // VISTAS
 
-    return fechaString;
+  calcularRangoSemana(fecha: Date) : {fechaInicio: Date, fechaFin: Date} {
+    const fechaInicio = new Date(fecha);
+    fechaInicio.setDate(fecha.getDate()- fecha.getDay() + 1);
+    const fechaFin = new Date(fechaInicio);
+    fechaFin.setDate(fechaInicio.getDate() + 6);
+
+    return {fechaInicio, fechaFin};
+
+  }
+
+  calcularRangoMes(fecha: Date): {fechaInicio: Date, fechaFin: Date} {
+    const fechaInicio = new Date(fecha.getFullYear(), fecha.getMonth(), 1);
+    const fechaFin = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0);
+
+    return {fechaInicio, fechaFin};
   }
 }
